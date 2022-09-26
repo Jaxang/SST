42b4b2e38d2e5eb5c2365b988a339e7a98936c11
Georg Hess <georg.hess@zenseact.com>
2022-09-26 11:07:38 +0200

    adds option for normalizing decoder input feats

--- configs/centerpoint/centerpoint_01voxel_second_secfpn_4x8_cyclic_20e_nus_masked.py
+++ configs/centerpoint/centerpoint_01voxel_second_secfpn_4x8_cyclic_20e_nus_masked.py
@@ -78,9 +78,10 @@ model = dict(
         debug=False,
         use_fake_voxels=use_fake_voxels,
         in_channel=512,
-        layer_cfg=dict(
-            post_norm=False,
-        )
+        normalize_input=True,
+        #layer_cfg=dict(
+        #    post_norm=False,
+        #)
     ),
 
     pts_bbox_head=dict(
